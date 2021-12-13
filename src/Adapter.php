e15e8429b4a81689c74eb4c1aa69f3b587cc580d
Raymond Chong <mathsgod@yahoo.com>
2021-12-13 17:22:51 +0800

    update

--- src/Adapter.php
+++ src/Adapter.php
@@ -14,6 +14,10 @@ class Adapter implements FilesystemAdapter
     private $client;
     function __construct(string $key, string $server)
     {
+        if (!str_ends_with($server, '/')) {
+            $server .= '/';
+        }
+
         $this->client = new Client([
             'base_uri' => $server,
             "verify" => false,
@@ -26,12 +30,19 @@ class Adapter implements FilesystemAdapter
 
     function fileExists(string $path): bool
     {
+        if (str_starts_with($path, '/')) {
+            $path = substr($path, 1);
+        }
         $response = $this->client->head($path);
         return $response->getStatusCode() == 200;
     }
 
     function write(string $path, string $contents, Config $config): void
     {
+        if (str_starts_with($path, '/')) {
+            $path = substr($path, 1);
+        }
+        
         $this->client->put($path, [
             'body' => $contents
         ]);
@@ -39,17 +50,29 @@ class Adapter implements FilesystemAdapter
 
     function writeStream(string $path, $contents, Config $config): void
     {
+        if (str_starts_with($path, '/')) {
+            $path = substr($path, 1);
+        }
+
         $this->write($path, (string)stream_get_contents($contents), $config);
     }
 
     function read(string $path): string
     {
+        if (str_starts_with($path, '/')) {
+            $path = substr($path, 1);
+        }
+
         $response = $this->client->get($path);
         return $response->getBody()->getContents();
     }
 
     function readStream(string $path)
     {
+        if (str_starts_with($path, '/')) {
+            $path = substr($path, 1);
+        }
+
         $stream = fopen('php://memory', 'r+');
         fwrite($stream, $this->read($path));
         rewind($stream);
@@ -58,11 +81,19 @@ class Adapter implements FilesystemAdapter
 
     function delete(string $path): void
     {
+        if (str_starts_with($path, '/')) {
+            $path = substr($path, 1);
+        }
+
         $this->client->delete($path);
     }
 
     function deleteDirectory(string $path): void
     {
+        if (str_starts_with($path, '/')) {
+            $path = substr($path, 1);
+        }
+
         if (!str_ends_with($path, '/')) {
             $path .= "/";
         }
@@ -71,6 +102,10 @@ class Adapter implements FilesystemAdapter
 
     function createDirectory(string $path, Config $config): void
     {
+        if (str_starts_with($path, '/')) {
+            $path = substr($path, 1);
+        }
+
         if (!str_ends_with($path, '/')) {
             $path .= "/";
         }
@@ -83,11 +118,18 @@ class Adapter implements FilesystemAdapter
 
     function visibility(string $path): FileAttributes
     {
+        if (str_starts_with($path, '/')) {
+            $path = substr($path, 1);
+        }
         return new FileAttributes($path, null, "private", null, null);
     }
 
     function mimeType(string $path): FileAttributes
     {
+        if (str_starts_with($path, '/')) {
+            $path = substr($path, 1);
+        }
+
         $response = $this->client->head($path);
         $mimeType = $response->getHeaderLine("Content-Type");
         return  new FileAttributes($path, null, null, null, $mimeType);
@@ -95,6 +137,10 @@ class Adapter implements FilesystemAdapter
 
     function lastModified(string $path): FileAttributes
     {
+        if (str_starts_with($path, '/')) {
+            $path = substr($path, 1);
+        }
+
         $response = $this->client->head($path);
         $lastModified = $response->getHeaderLine("Last-Modified");
         return new FileAttributes($path, null, null, $lastModified, null);
@@ -102,6 +148,10 @@ class Adapter implements FilesystemAdapter
 
     function fileSize(string $path): FileAttributes
     {
+        if (str_starts_with($path, '/')) {
+            $path = substr($path, 1);
+        }
+
         $response = $this->client->head($path);
         $fileSize = $response->getHeaderLine("Content-Length");
         return new FileAttributes($path, $fileSize, null, null, null,);
@@ -109,8 +159,8 @@ class Adapter implements FilesystemAdapter
 
     function listContents(string $path, bool $deep): iterable
     {
-        if (!str_ends_with($path, '/')) {
-            $path .= "/";
+        if (str_starts_with($path, '/')) {
+            $path = substr($path, 1);
         }
 
         $response = $this->client->get($path);
@@ -129,6 +179,14 @@ class Adapter implements FilesystemAdapter
 
     function move(string $source, string $destination, Config $config): void
     {
+        if (str_starts_with($source, '/')) {
+            $source = substr($source, 1);
+        }
+
+        if (str_starts_with($destination, '/')) {
+            $destination = substr($destination, 1);
+        }
+
         $this->client->request('MOVE', $source, [
             'headers' => [
                 'Destination' => $destination
@@ -138,6 +196,14 @@ class Adapter implements FilesystemAdapter
 
     function copy(string $source, string $destination, Config $config): void
     {
+        if (str_starts_with($source, '/')) {
+            $source = substr($source, 1);
+        }
+
+        if (str_starts_with($destination, '/')) {
+            $destination = substr($destination, 1);
+        }
+
         $this->client->request('COPY', $source, [
             'headers' => [
                 'Destination' => $destination
